917ea26aed3ba631ebc62dfea122a0c2f27c4652
Romen-Dev <romen_adama.c.r@outlook.com>
2023-12-16 19:03:46 +0000

    Actualizacion del repositorio en el día 16-12-2023 de las prácticas de la Asignatura PI

---
+++ update2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Obtén la fecha del sistema en el formato deseado (YYYY-MM-DD)
+fecha_del_dia=$(date +'%d-%m-%Y')
+
+# Directorio raíz del repositorio
+repositorio_dir="/w/Universidad/PI/Practicas-PI"
+
+# Mensaje de commit personalizado
+mensaje_commit="Actualizacion del repositorio en el día $fecha_del_dia de las prácticas de la Asignatura PI"
+
+# Cambia al directorio del repositorio
+cd "$repositorio_dir" || exit
+
+# Agrega todos los cambios en el repositorio (incluyendo subdirectorios)
+git add --all
+
+# Realiza el commit con el mensaje personalizado
+git commit -m "$mensaje_commit"
+
+# Sube los cambios al repositorio remoto en la rama 'main' 
+git push origin final
